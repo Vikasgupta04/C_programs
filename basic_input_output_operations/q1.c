cf3f1c5192b616e42bd4f52714aaf5fadadd594e
Vikas Gupta <vg482002@gmail.com>
2025-01-08 00:06:59 +0530

    added question 1

---
+++ basic_input_output_operations/q1.c
@@ -0,0 +1,10 @@
+// Write a program to input and add two numbers
+#include <stdio.h>
+int main()
+{
+    int a, b;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+    printf("Sum of %d and %d is %d\n", a, b, a + b);
+    return 0;
+}
